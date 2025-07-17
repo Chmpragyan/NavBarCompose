c9eb51a3d190a365481a0790393cbcc92c3a1909
Pragyan Chamlagain <pragyan.chamlagain@codavatar.tech>
2025-07-17 12:24:06 +0545

    Feat DIAL-10755 [Work In Progress]: Design OTP input UI and logic to input digit with change in focused box color.

--- app/src/main/java/com/example/bottomnav/presentation/screens/auth/SecurityCodeSignInScreen.kt
+++ app/src/main/java/com/example/bottomnav/presentation/screens/auth/SecurityCodeSignInScreen.kt
@@ -1,18 +1,36 @@
 package com.example.bottomnav.presentation.screens.auth
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bottomnav.R
 
 @Preview
@@ -35,6 +53,75 @@ fun SecurityCodeSignInScreen() {
                 textColor = colorResource(R.color.on_background),
                 spaceAfterIcon = 16.dp
             )
+            Spacer(modifier = Modifier.height(32.dp))
+            Text("Enter Security Code")
+            Spacer(modifier = Modifier.height(12.dp))
+            OtpField()
         }
+    }
+}
+
+@Composable
+fun OtpField() {
+    var otpValue by remember { mutableStateOf("") }
+
+    BasicTextField(
+        value = otpValue,
+        onValueChange = {
+            if (it.length <= 6) {
+                otpValue = it
+            }
+        },
+        keyboardOptions = KeyboardOptions(
+            showKeyboardOnFocus = true,
+            keyboardType = KeyboardType.Number
+        ),
+        decorationBox = {
+            Row {
+                repeat(6) { index ->
+                    val char = when {
+                        index >= otpValue.length -> ""
+                        else -> otpValue[index].toString()
+                    }
+                    val isFocused = index == otpValue.length
+
+                    OtpBox(
+                        modifier = Modifier
+                            .width(40.dp)
+                            .height(40.dp)
+                            .background(Color.Transparent),
+                        text = char,
+                        isFocused = isFocused
+                    )
+                    Spacer(Modifier.width(8.dp))
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun OtpBox(
+    modifier: Modifier,
+    text: String,
+    isFocused: Boolean
+) {
+    Box(
+        modifier = modifier
+            .border(
+                2.dp,
+                if (isFocused) colorResource(R.color.primary) else colorResource(R.color.pin_box_color),
+                RoundedCornerShape(8.dp)
+            ),
+    ) {
+        Text(
+            text = text,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.W500,
+            style = MaterialTheme.typography.headlineMedium,
+            color = Color.DarkGray,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+        )
     }
 }
