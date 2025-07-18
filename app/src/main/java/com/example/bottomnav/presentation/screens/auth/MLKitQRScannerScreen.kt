82045f507a5efc7e4a148ac1f247efe23578048b
Pragyan Chamlagain <pragyan.chamlagain@codavatar.tech>
2025-07-18 15:58:34 +0545

    Feat DIAL-10754 [Work In Progress]: Add the cutout shape as to scan qr and implement it.

--- app/src/main/java/com/example/bottomnav/presentation/screens/auth/MLKitQRScannerScreen.kt
+++ app/src/main/java/com/example/bottomnav/presentation/screens/auth/MLKitQRScannerScreen.kt
@@ -8,38 +8,52 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import com.example.bottomnav.R
+import com.example.bottomnav.presentation.components.CutOutShape
 import com.example.bottomnav.utils.QRCodeAnalyzer
 import java.util.concurrent.Executors
 
 @Composable
 fun MLKitQRScannerScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        CameraPreview()
-        Text(
-            text = "Scan QR Code",
-            modifier = Modifier.padding(top = 48.dp)
-        )
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            CameraPreview(modifier = Modifier.fillMaxSize())
+
+//            QRScannerTargetBackground(modifier = Modifier.fillMaxSize())
+
+            BackIconButton(
+                modifier = Modifier.padding(top = 12.dp, start = 4.dp), onTap = {}
+            )
+        }
     }
 }
 
 @Composable
-fun CameraPreview() {
+fun CameraPreview(modifier: Modifier) {
+    val configuration = LocalConfiguration.current
+    val height = configuration.screenHeightDp.dp
     AndroidView(
         { context ->
             val cameraExecutor = Executors.newSingleThreadExecutor()
@@ -83,6 +97,21 @@ fun CameraPreview() {
                 }
             }, ContextCompat.getMainExecutor(context))
             previewView
-        }, modifier = Modifier.size(height = 200.dp, width = 200.dp)
+        }, modifier = Modifier
+            .clip(CutOutShape(height / 1.2f))
     )
+}
+
+@Composable
+private fun BackIconButton(
+    modifier: Modifier = Modifier, onTap: () -> Unit
+) {
+    Icon(painter = painterResource(R.drawable.ic_back),
+        contentDescription = "Back",
+        tint = MaterialTheme.colorScheme.surface,
+        modifier = modifier
+            .size(48.dp)
+            .background(Color.Transparent)
+            .padding(12.dp)
+            .clickable { onTap() })
 }
