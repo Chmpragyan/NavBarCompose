79419fd94b85a2f32d220bb9ad3e2afdee93f240
Pragyan Chamlagain <pragyan.chamlagain@codavatar.tech>
2025-07-18 08:51:20 +0545

    Feat DIAL-10754 [Work In Progress]: Add required dependencies and camera permission.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,13 @@ dependencies {
 
     // Navigation
     implementation(libs.androidx.navigation.compose)
+
+    // ML Kit Barcode Scanning
+    implementation (libs.barcode.scanning)
+
+    // Camera
+    implementation (libs.androidx.camera.core)
+    implementation (libs.androidx.camera.camera2)
+    implementation (libs.androidx.camera.lifecycle)
+    implementation (libs.androidx.camera.view)
 }
